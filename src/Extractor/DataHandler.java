4c696fe045ca7c800beb124522f4e75f357ca6a4
Yeri <kailynx@gmail.com>
2014-10-23 22:56:22 +1100

    filter out weekly file for the current week

--- src/Extractor/DataHandler.java
+++ src/Extractor/DataHandler.java
@@ -1,41 +1,60 @@
 package Extractor;
+
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class DataHandler {
 
 	private static final String DESTINATION = "/Users/cse/Thesis/unzipTester";
-	private ArrayList<File> dataFiles;
 
 	public DataHandler() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ArrayList<File> getExtractedData() {
-		return dataFiles;
+	public static ArrayList<File> getExtractedData() {
+		ArrayList<File> files = new ArrayList<File>();
+		File folder = new File("/Users/cse/Thesis/data");
+		File[] listOfFiles = folder.listFiles();
+		File init = null;
+		File patch = null;
+		File weekly = null;
+	    for (int i = 0; i < listOfFiles.length; i++) {
+	    	File curr = listOfFiles[i];
+	      if (curr.isFile() &&  curr.getName().equals("simo.init.zip")) {
+	    	  init = curr;
+	      } else if (curr.isFile() &&  curr.getName().equals("simo.patch.zip")) {
+	    	  patch = curr;
+	      } else if (curr.isFile() &&  curr.getName().equals("simo." + getCurrentSunday() + ".zip")) {
+	    	  weekly = curr;
+	      }
+	    }
+	    files.add(weekly);
+	    files.add(init);
+	    files.add(patch);
+	    return files;
 	}
 	
 	public static ArrayList<File> extractData (File source) {
 		System.out.println("Start ExtractData");
 		File zipFile = source;
-		ArrayList<File> files = new ArrayList<File>();
 		ZipHandler zipHandler = new ZipHandler();
 		ArrayList<File> filePaths = zipHandler.unZip(zipFile.getAbsolutePath(), DESTINATION);
 		ParserHandler parserHandler = new ParserHandler();
 		parserHandler.parseFiles(filePaths);
 		System.out.println("Database is generated");
-		File folder = new File("/Users/cse/Thesis/data");
-		File[] listOfFiles = folder.listFiles();
-	    for (int i = 0; i < listOfFiles.length; i++) {
-	    	File curr = listOfFiles[i];
-	      if (curr.isFile() &&  curr.getName().endsWith(".zip")) {
-	    	  files.add(curr);
-	      }
-	    }
-		return files;
+		return getExtractedData();
 	}
 
+	private static String getCurrentSunday() {
+		Calendar c = Calendar.getInstance();
+		c.setFirstDayOfWeek(Calendar.MONDAY);
+		c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+		return new SimpleDateFormat("yyyyMMdd").format(c.getTime());
+		
+	}
 	// Below stuffs are for testing purpose
 	private File data;
 	public File getExtractedDataTest() {
@@ -53,7 +72,7 @@ public class DataHandler {
 	}
 
 	public static void main (String args[]) {
-		File source = new File("/Users/cse/Thesis/transxchange-373.zip");
+		File source = new File("/Users/cse/Thesis/transxchange2.zip");
 		extractDataTest(source);
 	}
 }
